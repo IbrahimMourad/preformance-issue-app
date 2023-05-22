3ccde79915d27b3b9970f9dc48267dcfcbdf942c
Ibrahim Mourad <ibrahimm.mourad97@gmail.com>
2023-05-22 12:28:23 +0300

    Initial commit

---
+++ src/heavy-component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const wait = (ms: number) => {
+  const start = Date.now();
+  let now = start;
+  while (now - start < ms) now = Date.now();
+};
+
+export const VeryHeavyComponentNoProps = () => {
+  wait(1000);
+
+  return <></>;
+};
